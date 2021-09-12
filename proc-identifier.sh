56ee9fa5980fab1e25cc0f806bce811603b55fcc
Christian Cantarino <christian.cantarino@gmail.com>
2021-09-12 20:25:50 -0300

    minor fixes

--- proc-identifier.sh
+++ proc-identifier.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-E_WRONGARGS=65
-E_BADPID=66
-E_NOSUCHPROCESS=67
-E_NOPERMISSION=68
 PROCFILE=exe
-
-#func
-#atr var
-#par posicionais
-#expansao de cmd
-#fluxo cond inteiros
-#fluxo cond arq
-#filtrando saida
-#permissao de leitura
-#awk
-#for
 
 function fatal_error() {
     echo "[-] Erro: $@" 1>&2;
@@ -43,16 +28,16 @@ do
     proc_error "Nao tem nenhum processo com o id $proc executando."
   fi  
 
-  if [ ! -r "/proc/$1/$PROCFILE" ] 
+  if [ ! -r "/proc/$proc/$PROCFILE" ] 
   then
-    proc_error "O processo $proc esta sendo executado, \n     contudo nao possuo permissao para ler o seu arquivo /proc/$1/$PROCFILE."
+    proc_error "O processo $proc esta sendo executado, \n     contudo nao possuo permissao para ler o seu arquivo /proc/$proc/$PROCFILE."
   fi  
 
-  exe_file=$( ls -l /proc/$1 | grep "exe" | awk '{ print $11 }' )
+  exe_file=$( ls -l /proc/$proc | grep "exe" | awk '{ print $11 }' )
 
   if [ -e "$exe_file" ]  
   then                   
-    echo "Processo #$1 executado por $exe_file."
+    echo "Processo #$proc executado por $exe_file."
   else
     proc_error "Nao consegui identificar o arquivo."
   fi 
