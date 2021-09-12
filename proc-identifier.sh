667bf9708386cc07b0412f9aa1b09a2d67cc5f44
Christian Cantarino <christian.cantarino@gmail.com>
2021-09-12 20:38:18 -0300

    fix continue issues on linux

--- proc-identifier.sh
+++ proc-identifier.sh
@@ -9,7 +9,6 @@ function fatal_error() {
 
 function proc_error() {
   echo -e "   [-] Erro: $@" 1>&2;
-  continue;
 }
 
 if [ $# -eq 0 ]
@@ -26,18 +25,20 @@ do
   if [ -z "$pidno" ]  
   then                
     proc_error "Nao tem nenhum processo com o id $proc executando."
+    continue;
   fi  
 
   if [ ! -r "/proc/$proc/$PROCFILE" ] 
   then
     proc_error "O processo $proc esta sendo executado, \n     contudo nao possuo permissao para ler o seu arquivo /proc/$proc/$PROCFILE."
+    continue;
   fi  
 
   exe_file=$( ls -l /proc/$proc | grep "exe" | awk '{ print $11 }' )
 
   if [ -e "$exe_file" ]  
   then                   
-    echo "Processo #$proc executado por $exe_file."
+    echo "   Processo #$proc executado por $exe_file."
   else
     proc_error "Nao consegui identificar o arquivo."
   fi 
